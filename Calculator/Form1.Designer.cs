5fd3aece74c4aae89df25ab6e6e87451684d3d74
Никита Кириллов <kirillov.nru@students.dvfu.ru>
2021-03-03 20:22:04 +1000

    First

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.mainTable = new System.Windows.Forms.TextBox();
+            this.button0 = new System.Windows.Forms.Button();
+            this.divBtn = new System.Windows.Forms.Button();
+            this.mulBtn = new System.Windows.Forms.Button();
+            this.minBtn = new System.Windows.Forms.Button();
+            this.sumBtm = new System.Windows.Forms.Button();
+            this.resBtn = new System.Windows.Forms.Button();
+            this.pointBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.powBtn = new System.Windows.Forms.Button();
+            this.sqrtBtn = new System.Windows.Forms.Button();
+            this.inversionBtn = new System.Windows.Forms.Button();
+            this.deleteBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(11, 263);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(67, 263);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(123, 263);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button4.Location = new System.Drawing.Point(11, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(67, 208);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 50);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(123, 208);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 50);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button7.Location = new System.Drawing.Point(11, 151);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 50);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button8.Location = new System.Drawing.Point(67, 151);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 50);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button9.Location = new System.Drawing.Point(123, 151);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 50);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // mainTable
+            // 
+            this.mainTable.Location = new System.Drawing.Point(11, 10);
+            this.mainTable.Name = "mainTable";
+            this.mainTable.Size = new System.Drawing.Size(218, 23);
+            this.mainTable.TabIndex = 9;
+            this.mainTable.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // button0
+            // 
+            this.button0.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button0.Location = new System.Drawing.Point(11, 319);
+            this.button0.Name = "button0";
+            this.button0.Size = new System.Drawing.Size(106, 50);
+            this.button0.TabIndex = 10;
+            this.button0.Text = "0";
+            this.button0.UseVisualStyleBackColor = true;
+            this.button0.Click += new System.EventHandler(this.countButton_Click);
+            // 
+            // divBtn
+            // 
+            this.divBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.divBtn.Location = new System.Drawing.Point(179, 95);
+            this.divBtn.Name = "divBtn";
+            this.divBtn.Size = new System.Drawing.Size(50, 50);
+            this.divBtn.TabIndex = 11;
+            this.divBtn.Text = "/";
+            this.divBtn.UseVisualStyleBackColor = true;
+            this.divBtn.Click += new System.EventHandler(this.divBtn_Click);
+            // 
+            // mulBtn
+            // 
+            this.mulBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.mulBtn.Location = new System.Drawing.Point(179, 151);
+            this.mulBtn.Name = "mulBtn";
+            this.mulBtn.Size = new System.Drawing.Size(50, 50);
+            this.mulBtn.TabIndex = 12;
+            this.mulBtn.Text = "X";
+            this.mulBtn.UseVisualStyleBackColor = true;
+            this.mulBtn.Click += new System.EventHandler(this.mulBtn_Click);
+            // 
+            // minBtn
+            // 
+            this.minBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.minBtn.Location = new System.Drawing.Point(179, 207);
+            this.minBtn.Name = "minBtn";
+            this.minBtn.Size = new System.Drawing.Size(50, 50);
+            this.minBtn.TabIndex = 13;
+            this.minBtn.Text = "-";
+            this.minBtn.UseVisualStyleBackColor = true;
+            this.minBtn.Click += new System.EventHandler(this.minBtn_Click);
+            // 
+            // sumBtm
+            // 
+            this.sumBtm.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sumBtm.Location = new System.Drawing.Point(179, 263);
+            this.sumBtm.Name = "sumBtm";
+            this.sumBtm.Size = new System.Drawing.Size(50, 50);
+            this.sumBtm.TabIndex = 14;
+            this.sumBtm.Text = "+";
+            this.sumBtm.UseVisualStyleBackColor = true;
+            this.sumBtm.Click += new System.EventHandler(this.sumBtm_Click);
+            // 
+            // resBtn
+            // 
+            this.resBtn.BackColor = System.Drawing.SystemColors.Control;
+            this.resBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.resBtn.Location = new System.Drawing.Point(179, 319);
+            this.resBtn.Name = "resBtn";
+            this.resBtn.Size = new System.Drawing.Size(50, 50);
+            this.resBtn.TabIndex = 15;
+            this.resBtn.Text = "=";
+            this.resBtn.UseVisualStyleBackColor = false;
+            this.resBtn.Click += new System.EventHandler(this.resBtn_Click);
+            // 
+            // pointBtn
+            // 
+            this.pointBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.pointBtn.Location = new System.Drawing.Point(123, 319);
+            this.pointBtn.Name = "pointBtn";
+            this.pointBtn.Size = new System.Drawing.Size(50, 50);
+            this.pointBtn.TabIndex = 16;
+            this.pointBtn.Text = ".";
+            this.pointBtn.UseVisualStyleBackColor = true;
+            this.pointBtn.Click += new System.EventHandler(this.pointBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.clearBtn.Location = new System.Drawing.Point(11, 39);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(50, 50);
+            this.clearBtn.TabIndex = 17;
+            this.clearBtn.Text = "C";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // powBtn
+            // 
+            this.powBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.powBtn.Location = new System.Drawing.Point(67, 95);
+            this.powBtn.Name = "powBtn";
+            this.powBtn.Size = new System.Drawing.Size(50, 50);
+            this.powBtn.TabIndex = 18;
+            this.powBtn.Text = "x^y";
+            this.powBtn.UseVisualStyleBackColor = true;
+            this.powBtn.Click += new System.EventHandler(this.powBtn_Click);
+            // 
+            // sqrtBtn
+            // 
+            this.sqrtBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.sqrtBtn.Location = new System.Drawing.Point(123, 95);
+            this.sqrtBtn.Name = "sqrtBtn";
+            this.sqrtBtn.Size = new System.Drawing.Size(50, 50);
+            this.sqrtBtn.TabIndex = 19;
+            this.sqrtBtn.Text = "sqrt";
+            this.sqrtBtn.UseVisualStyleBackColor = true;
+            this.sqrtBtn.Click += new System.EventHandler(this.sqrtBtn_Click);
+            // 
+            // inversionBtn
+            // 
+            this.inversionBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.inversionBtn.Location = new System.Drawing.Point(11, 95);
+            this.inversionBtn.Name = "inversionBtn";
+            this.inversionBtn.Size = new System.Drawing.Size(50, 50);
+            this.inversionBtn.TabIndex = 20;
+            this.inversionBtn.Text = "+/-";
+            this.inversionBtn.UseVisualStyleBackColor = true;
+            this.inversionBtn.Click += new System.EventHandler(this.inversionBtn_Click);
+            // 
+            // deleteBtn
+            // 
+            this.deleteBtn.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.deleteBtn.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.deleteBtn.Location = new System.Drawing.Point(123, 39);
+            this.deleteBtn.Name = "deleteBtn";
+            this.deleteBtn.Size = new System.Drawing.Size(106, 50);
+            this.deleteBtn.TabIndex = 21;
+            this.deleteBtn.Text = "DEL";
+            this.deleteBtn.UseVisualStyleBackColor = true;
+            this.deleteBtn.Click += new System.EventHandler(this.deleteBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(238, 378);
+            this.Controls.Add(this.deleteBtn);
+            this.Controls.Add(this.inversionBtn);
+            this.Controls.Add(this.sqrtBtn);
+            this.Controls.Add(this.powBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.pointBtn);
+            this.Controls.Add(this.resBtn);
+            this.Controls.Add(this.sumBtm);
+            this.Controls.Add(this.minBtn);
+            this.Controls.Add(this.mulBtn);
+            this.Controls.Add(this.divBtn);
+            this.Controls.Add(this.button0);
+            this.Controls.Add(this.mainTable);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox mainTable;
+        private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button divBtn;
+        private System.Windows.Forms.Button mulBtn;
+        private System.Windows.Forms.Button minBtn;
+        private System.Windows.Forms.Button sumBtm;
+        private System.Windows.Forms.Button resBtn;
+        private System.Windows.Forms.Button pointBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button powBtn;
+        private System.Windows.Forms.Button sqrtBtn;
+        private System.Windows.Forms.Button inversionBtn;
+        private System.Windows.Forms.Button deleteBtn;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private bool _flag;
+        private bool _firstSymbol;
+        private bool _pointFlag;
+        private bool _divFlag;
+        private bool _mulFlag;
+        private bool _sumFlag;
+        private bool _minFlag;
+        private bool _powFlag;
+        private bool _sqrtFlag;
+        private bool _negative;
+        private double _firstCount;
+        private double _secondCount;
+        private double res;
+        public Form1()
+        {
+            InitializeComponent();
+            mainTable.Text = "0";
+            _firstSymbol = true;
+            _flag = false;
+            _divFlag = false;
+            _mulFlag = false;
+            _sumFlag = false;
+            _minFlag = false;
+            _powFlag = false;
+            _sqrtFlag = false;
+            _pointFlag = false;
+            _negative = false;
+        }
+
+        private void countButton_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            if (_firstSymbol)
+            {
+                mainTable.Text = $"{btn.Text}";
+                _firstSymbol = false;
+            }
+            else
+            {
+                mainTable.Text += $"{btn.Text}";
+            }
+        }
+
+        private void divBtn_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _divFlag = true;
+        }
+
+        private void minBtn_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _minFlag = true;
+        }
+
+        private void mulBtn_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _mulFlag = true;
+        }
+
+        private void sumBtm_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _sumFlag = true;
+        }
+
+        private void resBtn_Click(object sender, EventArgs e)
+        {
+            if (_flag)
+            {
+                _secondCount = Convert.ToDouble(mainTable.Text);
+                if (_divFlag && _secondCount != 0)
+                {
+                    res = _firstCount / _secondCount;
+                    _divFlag = false;
+                }
+                else if (_sumFlag)
+                {
+                    res = _firstCount + _secondCount;
+                    _sumFlag = false;
+                }
+                else if (_minFlag)
+                {
+                    res = _firstCount - _secondCount;
+                    _minFlag = false;
+                }
+                else if (_mulFlag)
+                {
+                    res = _firstCount * _secondCount;
+                    _mulFlag = false;
+                }
+                else if (_powFlag)
+                {
+                    res = Math.Pow(_firstCount, _secondCount);
+                    _powFlag = false;
+                }
+                else if (_sqrtFlag)
+                {
+                    res = Math.Pow(_firstCount, (1 / _secondCount));
+                    _sqrtFlag = false;
+                }
+            }
+            mainTable.Text = $"{res}";
+            _flag = false;
+            _firstCount = res;
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            mainTable.Text = "0";
+            _firstCount = 0;
+            _secondCount = 0;
+            _firstSymbol = true;
+        }
+
+        private void powBtn_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _powFlag = true;
+        }
+
+        private void pointBtn_Click(object sender, EventArgs e)
+        {
+            if (!_pointFlag)
+            {
+                mainTable.Text += ',';
+                _pointFlag = true;
+                if (mainTable.Text == "0,") 
+                {
+                    _firstSymbol = false;
+                }
+            }
+        }
+
+        private void _firstCountFill()
+        {
+            if (!_flag)
+            {
+                _firstCount = Convert.ToDouble(mainTable.Text);
+                mainTable.Text = "0";
+                _flag = true;
+                _firstSymbol = true;
+                _pointFlag = false;
+            }
+        }
+
+        private void deleteBtn_Click(object sender, EventArgs e)
+        {
+            if (mainTable.Text.Length != 0)
+            {
+                mainTable.Text = mainTable.Text.Remove(mainTable.Text.Length - 1, 1);
+                if (!mainTable.Text.Contains(',')) 
+                {
+                    _pointFlag = false;
+                }
+            }
+        }
+
+        private void sqrtBtn_Click(object sender, EventArgs e)
+        {
+            _firstCountFill();
+            _sqrtFlag = true;
+        }
+
+        private void inversionBtn_Click(object sender, EventArgs e)
+        {
+            if (_negative)
+            {
+                mainTable.Text = mainTable.Text.Remove(0, 1);
+                _negative = false; 
+            }
+            else 
+            {
+                mainTable.Text=mainTable.Text.Insert(0, "-");
+                _negative = true;
+            }
+        }
+    }
+}
